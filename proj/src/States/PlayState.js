518415e47987dffeffc47d1dc1fcd63e72d3531e
Quinton Woodmansee <qwoodmansee@capital.edu>
2015-10-24 12:37:17 -0400

    - changed colors of block, button and background - created canMove properties, used in keyboard input and edge detection - removed useless code from flappy bird mock

--- proj/src/States/PlayState.js
+++ proj/src/States/PlayState.js
@@ -19,60 +19,21 @@ var PlayState = new Kiwi.State('PlayState');
 PlayState.create = function () {
 
 
+
     //Switch the background colour back to white from purple
-    this.game.stage.color = '999999';
+    this.game.stage.color = 'FFEDD1';
 
     //Variables relating to the state of the game
     this.gameStarted = false;
-    this.gameEnded = false;
 
     //Values pertaining to physics based logic 
-    this.gravity = 1;
-    this.xSpeed = 3;
-    this.ySpeed = 0;
-    this.maxYSpeed = 16;
+    this.xAcceleration = 3;
+    this.maxXSpeed = 9;
 
-    //speed that pushing a button makes
-    this.moveSpeed = -5;
-
-
-    this.flapSpeed = -16;
-    this.pipeDistance = 200;
-    this.pipeGap = 600;
 
     //The starting coordinates of the box.
     this.startX = (this.game.stage.width / 2);
     this.startY = (this.game.stage.height) - 200;
-
-    //Background image
-    //this.bg = new Kiwi.GameObjects.Sprite(this, this.textures.background, 0, 0);
-    //this.addChild(this.bg);
-
-   /* //Obstacles
-    this.pipes = [];
-    for (var j = 0; j < 3; j++) {
-        var topPipe = new Kiwi.GameObjects.Sprite(this, this.textures.pipe, j * this.pipeDistance + 400 + this.game.stage.width, -Math.random() * 300 - 400);
-        this.addChild(topPipe);
-        topPipe.transform.rotation += Math.PI;
-        this.pipes.push(topPipe);
-
-        var bottomPipe = new Kiwi.GameObjects.Sprite(this, this.textures.pipe, j * this.pipeDistance + 400 + this.game.stage.width, topPipe.transform.y + topPipe.height + this.pipeGap);
-        this.addChild(bottomPipe);
-        bottomPipe.passed = false;
-        bottomPipe.myTop = topPipe;
-        this.pipes.push(bottomPipe);
-    }*/
-
-    //Scrolling floor
-    /*this.tileWidth = 37;
-    this.tiles = [];
-    var tileCount = Math.ceil(this.game.stage.width / this.tileWidth);
-    for (var i = 0; i <= tileCount;ß i++) {
-        var t = new Kiwi.GameObjects.Sprite(this, this.textures.ground, i * this.tileWidth, this.game.stage.height - 128);
-        this.addChild(t);
-        this.tiles.push(t);
-    }
-*/
 
 
 
@@ -83,17 +44,17 @@ PlayState.create = function () {
         height: 100,
         centerOnTransform: true,
         x: this.startX,
-        y: this.startY
+        y: this.startY,
+        color: [17/255, 91/255, 137/255]
     });
 
     this.playerBox.velocity = 0;
-
+    this.playerBox.canMoveLeft = true;
+    this.playerBox.canMoveRight = true;
 
 
     this.addChild(this.playerBox);
 
-    //Input event for the flapper
-    //this.game.input.onDown.add(this.flap, this);
 
     //create key objects for input
     this.DKey = this.game.input.keyboard.addKey( Kiwi.Input.Keycodes.D);
@@ -104,19 +65,10 @@ PlayState.create = function () {
     this.KKey = this.game.input.keyboard.addKey( Kiwi.Input.Keycodes.K);
 
 
-    //create, then hide buttons
-    this.buttonX = this.game.stage.width / 2 - 107;
-    this.restartBtn = new Kiwi.GameObjects.Sprite(this, this.textures.restart, -217, this.game.stage.height / 2 - 150, true);
-    this.addChild(this.restartBtn);
-    this.restartBtn.input.onDown.add(this.restartGame, this);
-
-    this.shareBtn = new Kiwi.GameObjects.Sprite(this, this.textures.share, -217, this.game.stage.height / 2 - 50, true);
-    this.addChild(this.shareBtn);
-    this.shareBtn.input.onDown.add(this.shareGame, this);
 
     //Score
     this.score = 0;
-    this.scoreText = new Kiwi.GameObjects.Textfield(this, '0', 50, 50, '#FFF');
+    this.scoreText = new Kiwi.GameObjects.Textfield(this, '0', 50, 50, '#000');
     this.addChild(this.scoreText);
 }
 
@@ -126,17 +78,19 @@ Move box methods
 
 PlayState.moveBox = function (direction, box) {
 
+    //first keypress should start game
     this.gameStarted = true;
 
+    //move the box left
     if (direction == "left") {
 
-        box.velocity = this.moveSpeed;
+        if (Math.abs(box.velocity) < this.maxXSpeed) box.velocity -= this.xAcceleration;
 
     }
 
     else if (direction == "right") {
 
-        box.velocity = this.moveSpeed * -1;
+        if (Math.abs(box.velocity) < this.maxXSpeed) box.velocity += this.xAcceleration;
 
     }
 
@@ -161,56 +115,27 @@ PlayState.moveBox = function (direction, box) {
 
     }
 
-};
+    box.transform.x += box.velocity;
 
 
-/**
-* This method is executed when the player resets the game.
-* @method restartGame
-* @public
-*/
-PlayState.restartGame = function () {
-    this.gameStarted = false;
-    this.gameEnded = false;
-    this.score = 0;
-    this.scoreText.text = this.score;
+    box.canMoveRight = true;
+    box.canMoveLeft = true;
 
-    for (var p in this.pipes) {
-        var pipe = this.pipes[p];
-        pipe.transform.x += 400 + this.game.stage.width;
-        if (pipe.transform.rotation == 0) pipe.passed = false;
+    if ((box.x - box.width/2) < 0) {
+
+        box.canMoveLeft = false;
+        box.velocity = 0;
+
+
     }
 
-    this.playerBox.transform.x = this.startX;
-    this.playerBox.transform.y = this.startY;
-    this.playerBox.transform.rotation = 0;
+    if ((box.x + box.width/2) > this.game.stage.width) {
 
-    this.restartBtn.transform.x = -217;
-    this.shareBtn.transform.x = -217;
-};
+        box.canMoveRight = false;
+        box.velocity = 0;
 
+    }
 
-/**
-* This method is executed when the user presses the share game button.
-* @method shareGame
-* @public
-*/
-PlayState.shareGame = function () {
-    //Custom code relating to how you want to 'share' the game will need to go here.
-    //Right now we are just going to redirect the user to kiwi.js.org :P
-    window.open("http://www.kiwijs.org");
-};
-
-
-
-/**
-* This method is executed when the user clicks.
-* @method flap
-* @public
-*/
-PlayState.flap = function () {
-    this.gameStarted = true;
-    this.ySpeed = this.flapSpeed;
 };
 
 
@@ -219,11 +144,11 @@ PlayState.flap = function () {
  */
 PlayState.checkInput = function() {
 
-    if (this.GKey.isDown) {
+    if (this.GKey.isDown && this.playerBox.canMoveLeft) {
         this.moveBox("left", this.playerBox);
     }
 
-    else if (this.HKey.isDown) {
+    else if (this.HKey.isDown && this.playerBox.canMoveRight) {
         this.moveBox("right", this.playerBox);
     }
 
@@ -236,7 +161,6 @@ PlayState.checkInput = function() {
 };
 
 
-
 /**
 * This method is the main update loop. Move scrolling items here
 * @method update
@@ -247,53 +171,13 @@ PlayState.update = function () {
 
     if (this.gameStarted) {
 
-        if (this.playerBox.transform.x < this.game.stage.width) {
-
-            this.playerBox.transform.x += this.playerBox.velocity;
-
-        }
+        this.playerBox.transform.x += this.playerBox.velocity;
 
     }
 
     //check for keyboard input and set box velocity based on key pressed
     this.checkInput();
 
-
-    //move tiles
-    if (!this.gameEnded) {
-        for (var t in this.tiles) {
-            var tile = this.tiles[t];
-            tile.transform.x -= this.xSpeed;
-            if (tile.transform.x <= -this.tileWidth) tile.transform.x += this.tileWidth * this.tiles.length;
-        }
-        if (this.gameStarted) {
-            //move pipes
-            for (var p in this.pipes) {
-                var pipe = this.pipes[p];
-                pipe.transform.x -= this.xSpeed;
-                if (pipe.transform.rotation == 0) {
-                    if (pipe.transform.x < -pipe.width) {
-                        pipe.transform.x += this.pipeDistance * 3;
-                        pipe.passed = false;
-                        pipe.myTop.transform.x = pipe.transform.x;
-                        var rand = -Math.random() * 300 - 400;
-                        pipe.myTop.transform.y = rand;
-                        pipe.transform.y = pipe.myTop.transform.y + pipe.myTop.height + this.pipeGap;
-                    }
-
-                    if (pipe.transform.x < this.playerBox.transform.x && !pipe.passed) {
-                        this.score++;
-                        this.scoreText.text = this.score;
-                        console.log('score:', this.score);
-                        pipe.passed = true;
-                    }
-                }
-
-
-
-            }
-        }
-    }
 };
 
 
