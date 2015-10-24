e56ef64dc22a2d985cc7dad33f3aa758dcdbacf6
kholland950 <kholland950@capital.edu>
2015-10-24 11:12:25 -0400

    Added makeshift start menu (temporary) Changed title name in index.html

--- proj/src/States/MenuState.js
+++ proj/src/States/MenuState.js
@@ -6,19 +6,23 @@ var MenuState = new Kiwi.State('MenuState');
 MenuState.create = function() {
     //Switch the background colour back to white from purple
     this.game.stage.color = '999999';
-    this.playButton = new Kiwi.GameObjects.Sprite(this, this.textures.restart, game.stage.width / 20, true);
-    this.addChild(this.playButton);
+
+    this.playButton = new Kiwi.HUD.Widget.MenuItem(this.game, "Play", 0 ,0);
+    this.playButton.style.color = 'white';
+    this.playButton.style.display = 'block';
+    this.playButton.style.boxSizing = 'border-box';
+    this.playButton.style.width = (this.menuWidth * 2).toString() + 'px';
+    this.playButton.style.textAlign = 'center';
+    this.playButton.style.cursor = 'pointer';
+    this.playButton.style.padding = '0.5em 1em';
+    this.playButton.style.backgroundColor = '#843';
+
+
+    this.menu = new Kiwi.HUD.Widget.Menu(this.game, game.stage.width/2, game.stage.width/2);
+    this.menu.addMenuItem(this.playButton);
+    this.game.huds.defaultHUD.addWidget(this.menu);
     this.playButton.input.onDown.add(function() {
         game.states.switchState("PlayState");
+        this.playButton.style.display = 'none';
     }, this);
 };
-
-
-/*
-
-//create, then hide buttons
-this.buttonX = this.game.stage.width / 2 - 107
-this.restartBtn = new Kiwi.GameObjects.Sprite(this, this.textures.restart, -217, this.game.stage.height / 2 - 150, true);
-this.addChild(this.restartBtn);
-this.restartBtn.input.onDown.add(this.restartGame, this);
-*/
