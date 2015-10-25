4c922093d70895bb37b8c43425ef23723d8c3ccc
kholland950 <kholland950@capital.edu>
2015-10-25 14:09:32 -0400

    Fixed excessive white space in PlayState

--- proj/src/States/PlayState.js
+++ proj/src/States/PlayState.js
@@ -1,23 +1,22 @@
 var PlayState = new Kiwi.State('PlayState');
 
 /**
-* The PlayState in the core state that is used in the game. 
-*
-* It is the state where majority of the functionality occurs 'in-game' occurs.
-* 
-*
-* @class playState
-* @extends State
-* @constructor
-*/
+ * The PlayState in the core state that is used in the game.
+ *
+ * It is the state where majority of the functionality occurs 'in-game' occurs.
+ *
+ *
+ * @class playState
+ * @extends State
+ * @constructor
+ */
 
 /**
-* This create method is executed when Kiwi Game reaches the boot stage of the game loop.
-* @method create
-* @public
-*/
+ * This create method is executed when Kiwi Game reaches the boot stage of the game loop.
+ * @method create
+ * @public
+ */
 PlayState.create = function () {
-
     //Switch the background colour back to white from purple
     this.game.stage.color = 'FFEDD1';
 
@@ -46,20 +45,19 @@ PlayState.create = function () {
     this.startY = (this.game.stage.height) - 70;
 
     //player boxes
-    this.playerBoxes =[];
-
+    this.playerBoxes = [];
 
     //create initial player box
-    this.playerBox = new Kiwi.Plugins.Primitives.Rectangle( {
+    this.playerBox = new Kiwi.Plugins.Primitives.Rectangle({
         state: this,
         width: 100,
         height: 100,
         centerOnTransform: true,
         x: this.startX,
         y: this.startY,
-        drawFill:  true,
+        drawFill: true,
         drawStroke: false,
-        color: [17/255, 91/255, 137/255]
+        color: [17 / 255, 91 / 255, 137 / 255]
     });
 
     this.playerBox.velocity = 0;
@@ -70,28 +68,22 @@ PlayState.create = function () {
 
     this.playerBoxes.push(this.playerBox);
 
-
     //enemies (FIFO queue)
     this.splitters = [];
 
-
     //create key objects for input
-    this.DKey = this.game.input.keyboard.addKey( Kiwi.Input.Keycodes.D);
-    this.FKey = this.game.input.keyboard.addKey( Kiwi.Input.Keycodes.F);
-    this.GKey = this.game.input.keyboard.addKey( Kiwi.Input.Keycodes.G);
-    this.HKey = this.game.input.keyboard.addKey( Kiwi.Input.Keycodes.H);
-    this.JKey = this.game.input.keyboard.addKey( Kiwi.Input.Keycodes.J);
-    this.KKey = this.game.input.keyboard.addKey( Kiwi.Input.Keycodes.K);
-
-
+    this.DKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.D);
+    this.FKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.F);
+    this.GKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.G);
+    this.HKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.H);
+    this.JKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.J);
+    this.KKey = this.game.input.keyboard.addKey(Kiwi.Input.Keycodes.K);
 
     //Score
     this.score = 0;
     this.scoreText = new Kiwi.GameObjects.Textfield(this, '0', 50, 50, '#000');
     this.addChild(this.scoreText);
-}
-
-
+};
 
 
 /**
@@ -106,15 +98,12 @@ PlayState.moveBox = function (direction, box) {
     //keep track of what time the game started
     this.gameStartTime = this.date.getTime();
 
-
     //move the box left
     if (direction == "left") {
         if (Math.abs(box.velocity) < this.maxXSpeed) box.velocity -= this.xAcceleration;
     } else if (direction == "right") {
         if (Math.abs(box.velocity) < this.maxXSpeed) box.velocity += this.xAcceleration;
-    }
-
-    else if (direction == "stop") {
+    } else if (direction == "stop") {
         if (box.velocity < 0) {
             box.velocity += this.friction;
         } else if (box.velocity > 0) {
@@ -130,11 +119,11 @@ PlayState.moveBox = function (direction, box) {
     box.canMoveRight = true;
     box.canMoveLeft = true;
 
-    if ((box.x - box.width/2) < 0 && direction == "left") {
+    if ((box.x - box.width / 2) < 0 && direction == "left") {
         box.canMoveLeft = false;
         box.velocity = 0;
     }
-    if ((box.x + box.width/2) > this.game.stage.width && direction == "right") {
+    if ((box.x + box.width / 2) > this.game.stage.width && direction == "right") {
         box.canMoveRight = false;
         box.velocity = 0;
     }
@@ -145,50 +134,33 @@ PlayState.moveBox = function (direction, box) {
  *  checks keyboard input and moves appropriate box.
  */
 
-PlayState.checkInput = function() {
-
+PlayState.checkInput = function () {
     //move left most box
     if (this.DKey.isDown && this.playerBoxes[0].canMoveLeft) {
         this.moveBox("left", this.playerBoxes[0]);
-    }
-
-    else if (this.FKey.isDown && this.playerBoxes[0].canMoveRight) {
+    } else if (this.FKey.isDown && this.playerBoxes[0].canMoveRight) {
         this.moveBox("right", this.playerBoxes[0]);
-    }
-
-    else if (this.playerBoxes[0].velocity != 0) {
-
+    } else if (this.playerBoxes[0].velocity != 0) {
         this.moveBox("stop", this.playerBoxes[0]);
     }
-
     //move middle/right box if two boxes
     if (this.playerBoxes.length > 1) {
         if (this.GKey.isDown && this.playerBoxes[1].canMoveLeft) {
             this.moveBox("left", this.playerBoxes[1]);
-        }
-
-        else if (this.HKey.isDown && this.playerBoxes[1].canMoveRight) {
+        } else if (this.HKey.isDown && this.playerBoxes[1].canMoveRight) {
             this.moveBox("right", this.playerBoxes[1]);
-        }
-
-        else if (this.playerBoxes[1].velocity != 0) {
+        } else if (this.playerBoxes[1].velocity != 0) {
 
             this.moveBox("stop", this.playerBoxes[1]);
         }
     }
-
     //move third box if 3 boxes
     if (this.playerBoxes.length > 2) {
         if (this.JKey.isDown && this.playerBoxes[2].canMoveLeft) {
             this.moveBox("left", this.playerBoxes[2]);
-        }
-
-        else if (this.KKey.isDown && this.playerBoxes[2].canMoveRight) {
+        } else if (this.KKey.isDown && this.playerBoxes[2].canMoveRight) {
             this.moveBox("right", this.playerBoxes[2]);
-        }
-
-        else if (this.playerBoxes[2].velocity != 0) {
-
+        } else if (this.playerBoxes[2].velocity != 0) {
             this.moveBox("stop", this.playerBoxes[2]);
         }
     }
@@ -199,25 +171,14 @@ PlayState.checkInput = function() {
  *
  */
 PlayState.moveSplitters = function (splitters) {
-
-    for (var i = 0; i<splitters.length; i++) {
-
+    for (var i = 0; i < splitters.length; i++) {
         //if splitter is on screen
         if (splitters[i].y - this.enemyHeight < this.game.stage.height) {
-
             splitters[i].transform.y += this.enemyFallSpeed;
-
-        }
-
-        else {
-
+        } else {
             splitters.shift().destroy();
-
-
         }
-
     }
-
 };
 
 /**
@@ -225,25 +186,20 @@ PlayState.moveSplitters = function (splitters) {
  *
  */
 PlayState.createEnemy = function () {
-
     //create a new enemy
-    this.splitter1 = new Kiwi.Plugins.Primitives.Triangle( {
-
+    this.splitter1 = new Kiwi.Plugins.Primitives.Triangle({
         state: this,
-        points: [ [0,0], [this.enemyWidth / 2, this.enemyHeight * -1], [this.enemyWidth / 2 * -1, this.enemyHeight * -1]],
+        points: [[0, 0], [this.enemyWidth / 2, this.enemyHeight * -1], [this.enemyWidth / 2 * -1, this.enemyHeight * -1]],
         x: Math.floor(Math.random() * this.game.stage.width),
         y: this.startY - 750,
-        color: [215/255, 2/255, 48/255],
-        strokeColor: [215/255, 2/255, 48/255],
+        color: [215 / 255, 2 / 255, 48 / 255],
+        strokeColor: [215 / 255, 2 / 255, 48 / 255],
         drawFill: true,
         drawStroke: true
-
     });
-
     this.addChild(this.splitter1);
     this.splitters.push(this.splitter1);
-
-}
+};
 
 /**
  * check to see if a box and a splitter collide. Returns true if they do
@@ -253,13 +209,10 @@ PlayState.createEnemy = function () {
  * @returns {boolean}
  */
 
-PlayState.checkBoxSplitterCollision = function(box, splitter) {
-
-    return (splitter.x > box.x - 1/2 * box.width) &&
-        (splitter.x < box.x + 1/2* box.width) &&
-        (splitter.y == box.y - 1/2* box.height);
-
-
+PlayState.checkBoxSplitterCollision = function (box, splitter) {
+    return (splitter.x > box.x - 1 / 2 * box.width) &&
+        (splitter.x < box.x + 1 / 2 * box.width) &&
+        (splitter.y == box.y - 1 / 2 * box.height);
 };
 
 /**
@@ -269,33 +222,25 @@ PlayState.checkBoxSplitterCollision = function(box, splitter) {
  * @returns {boolean}
  */
 
-PlayState.splitBox = function(box, posInArray) {
-
+PlayState.splitBox = function (box, posInArray) {
     // if the box can be split into 2
     if (box.weight > 1 && this.playerBoxes.length < 3) {
-
         //if the box is a 4 or 2 weight box
         if (box.weight % 2 == 0) {
-
             //remove the box from the array of boxes
             this.playerBoxes.splice(posInArray, 1);
 
             //create two new boxes
             this.createBox(box.weight / 2, box.x - box.width / 2);
             this.createBox(box.weight / 2, box.x + box.width / 2);
-
             //delete the box
             box.destroy();
-
         }
-
     }
-
     // if the box can be made into one smaller box
     else if (box.weight == 2) {
-
         //remove the box from the array of boxes
-        this.playerBoxes.splice(posInArray,1);
+        this.playerBoxes.splice(posInArray, 1);
 
         //create a new box
         this.createBox(box.weight / 2, box.x + box.x);
@@ -304,13 +249,11 @@ PlayState.splitBox = function(box, posInArray) {
         box.destroy();
     }
 
-
     // the box must be destroyed
     else {
         this.playerBoxes.splice(posInArray, 1);
         box.destroy();
     }
-
     //return false if there are no more boxes
     return (this.playerBoxes.length > 0)
 };
@@ -321,17 +264,16 @@ PlayState.splitBox = function(box, posInArray) {
  * @param xPos
  */
 PlayState.createBox = function (weight, xPos) {
-
-    this.playerBox = new Kiwi.Plugins.Primitives.Rectangle( {
+    this.playerBox = new Kiwi.Plugins.Primitives.Rectangle({
         state: this,
-        width: weight*25,
+        width: weight * 25,
         height: 100,
         centerOnTransform: true,
         x: xPos,
         y: this.startY,
-        drawFill:  true,
+        drawFill: true,
         drawStroke: false,
-        color: [17/255, 91/255, 137/255]
+        color: [17 / 255, 91 / 255, 137 / 255]
     });
 
     this.playerBox.velocity = 0;
@@ -341,40 +283,32 @@ PlayState.createBox = function (weight, xPos) {
     this.addChild(this.playerBox);
 
     this.playerBoxes.push(this.playerBox);
-
-
 };
 
 /**
-* This method is the main update loop. Move scrolling items here
-* @method update
-* @public
-*/
+ * This method is the main update loop. Move scrolling items here
+ * @method update
+ * @public
+ */
 PlayState.update = function () {
     Kiwi.State.prototype.update.call(this);
 
     if (this.gameStarted) {
-
         this.playerBox.transform.x += this.playerBox.velocity;
         this.moveSplitters(this.splitters);
 
         // figure out if a new enemy should spawn
-        if ((this.frameCount % 20 == 0 && this.frameCount != 0) && (Math.random()  <= .7)) {
-
+        if ((this.frameCount % 20 == 0 && this.frameCount != 0) && (Math.random() <= .7)) {
             this.createEnemy();
-
         }
 
         //check to see if any of the boxes hit any of the player boxes
         //for each block
         for (var j = 0; j < this.splitters.length; j++) {
             // for each player
-            for (var i = 0; i < this.playerBoxes.length; i++ ){
-
-
+            for (var i = 0; i < this.playerBoxes.length; i++) {
                 //check if they collide
                 if (this.checkBoxSplitterCollision(this.playerBoxes[i], this.splitters[j])) {
-
                     //if they do, split or destroy the box - if this is the last box  - GAME OVER
                     if (!this.splitBox(this.playerBoxes[i])) {
 
@@ -382,36 +316,23 @@ PlayState.update = function () {
                         game.states.switchState("MenuState");
 
                     }
-
                     // make it so no other boxes can be split this frame
                     j = 4;
                     i = 4;
 
                     //sort the player boxes so key presses do the correct thing
-                    this.playerBoxes.sort(function(a,b) {
+                    this.playerBoxes.sort(function (a, b) {
                         return a.x = b.x;
                     })
 
                 }
             }
-
         }
-
         //check to see if any player boxes collide
-
-
         this.frameCount++;
-
-
-
-
     }
-
     //check for keyboard input and set box velocity based on key pressed
     this.checkInput();
-
-
-
 };
 
 
